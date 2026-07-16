f85f5bfc57f880107e02f77a3d79293b6b85a695
Agustin Ledezma <agustinrobolero@gmail.com>
2026-07-16 09:45:32 -0400

    Se añadieron los set de usuario y rol

--- billing-app/src/main/java/com/diplomado/billing_app/model/Usuario.java
+++ billing-app/src/main/java/com/diplomado/billing_app/model/Usuario.java
@@ -23,6 +23,9 @@ public class Usuario {
 
     public Long getId() { return id; }
     public String getUsername() { return username; }
+    public void String
+    public void setUsername(String username) {this.username = username;}
     public String getPasswordHash() { return passwordHash; }
     public String getRol() { return rol; }
+    public void setRol(String rol) {this.rol = rol;}
 }
